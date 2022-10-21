f14c814c8431179802ce42c16627c970a7eb9dec
Thiago Salles <112223120+ThSFernandes@users.noreply.github.com>
2022-10-21 18:42:15 -0300

    Update Conta.java

--- Projeto Conta Bancária/Conta.java
+++ Projeto Conta Bancária/Conta.java
@@ -1,23 +1,40 @@
 
+package CriarConta;
+
 public class Conta {  // ( definindo atributos e comportamentos )
-    double saldo ;
+
+    double saldo;
     int numeroDaConta;
     int agencia;
     String titular;
-    
-    public void Deposita(double valor){
-        
-        saldo += 50;
-        
-    }
-    public boolean saca(double valor){
-        if(this.saldo >= valor){
-            this.saldo = this.saldo - valor;
+
+    public void Deposita(double valor) {
+
+        saldo += valor;
+            }
+
+    public boolean saca(double valor) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
             return true;
-        }
-        else{
+        } else {
             return false;
         }
     }
+
+    public boolean transfere(double valor, Conta destino) {
+
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+            destino.Deposita(valor);
+            return true;
+
+        } 
+            return false;
+
+    }
     
+    
+    
+
 }
