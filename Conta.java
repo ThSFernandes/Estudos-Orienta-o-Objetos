d6ce150a122d2da057d568c45a4c35f6376dd7e3
Thiago Salles <112223120+ThSFernandes@users.noreply.github.com>
2022-10-20 21:16:14 -0300

    Atualização 20/10/2022

--- Conta.java
+++ Conta.java
@@ -1,9 +1,23 @@
 
-
 public class Conta {  // ( definindo atributos e comportamentos )
     double saldo ;
     int numeroDaConta;
     int agencia;
     String titular;
+    
+    public void Deposita(double valor){
+        
+        saldo += 50;
+        
+    }
+    public boolean saca(double valor){
+        if(this.saldo >= valor){
+            this.saldo = this.saldo - valor;
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
     
 }
